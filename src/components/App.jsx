6381a2bb5f7be13d4efc534298715da232c9f457
Evgeniy Strelbitskiy <evgenstb@gmail.com>
2023-03-21 22:46:12 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Notiflix from 'notiflix';
+import SearchBar from './ImageFinder/Searchbar/Searchbar';
+import { FetchData } from './ImageFinder/API/API';
+import { ImageGallery } from './ImageFinder/ImageGallery/ImageGallery';
+import Button from './ImageFinder/Button/Button';
+import Loader from './ImageFinder/Loader/Loader';
+
+export class App extends Component {
+  state = {
+    searchReq: '',
+    page: 1,
+    images: [],
+    isLoading: false,
+    totalImg: 0,
+  };
+
+  async componentDidUpdate(_, prevState) {
+    const { searchReq, page } = this.state;
+
+    if (prevState.searchReq !== '' && prevState.searchReq !== searchReq) {
+      this.setState({ images: [] });
+    }
+    if (prevState.searchReq !== searchReq || prevState.page !== page) {
+      try {
+        this.setState({ isLoading: true });
+        await FetchData(searchReq, page)
+          .then(data => data.json())
+          .then(data => {
+            if (data.totalHits === 0) {
+              Notiflix.Notify.failure('No images matching your request');
+              this.setState({ isLoading: false });
+              return;
+            }
+
+            this.setState(prevState => ({
+              images:
+                page === 1 ? data.hits : [...prevState.images, ...data.hits],
+
+              totalImg:
+                page === 1
+                  ? data.totalHits - data.hits.length
+                  : data.totalHits - [...prevState.images, ...data.hits].length,
+            }));
+            this.setState({ isLoading: false });
+          });
+      } catch (error) {}
+    }
+  }
+
+  handledSearchBar = searchReq => {
+    this.setState({ searchReq, page: 1 });
+  };
+
+  changePage = () => {
+    this.setState(state => ({ page: state.page + 1 }));
+  };
+  render() {
+    return (
+      <>
+        <SearchBar onSubmit={this.handledSearchBar} />
+        {this.state.images && <ImageGallery images={this.state.images} />}
+        {this.state.isLoading && <Loader />}
+        {!!this.state.totalImg && <Button onClick={this.changePage} />}
+      </>
+    );
+  }
+}
---
+++ src/components/ImageFinder/Modal/Modal.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './Modal.css';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.closeByKey);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.closeByKey);
+  }
+
+  closeByKey = e => {
+    if (e.code !== 'Escape') {
+      return;
+    }
+    this.props.closeModal();
+  };
+
+  render() {
+    const { closeModal, tags, img } = this.props;
+    return createPortal(
+      <div className="Overlay" onClick={closeModal}>
+        <div className="Modal">
+          <img src={img} alt={tags} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+Modal.propTypes = {
+  img: PropTypes.string.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  tags: PropTypes.string.isRequired,
+};
